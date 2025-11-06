cabc5134b9873ebb93376dbf66d2b8569ed124c4
Al3k20 <alexandre.novaki@escola.pr.gov.br>
2025-11-06 10:39:12 -0300

    Update calculaPreco.py

--- calculaPreco.py
+++ calculaPreco.py
@@ -1,6 +1,7 @@
 # Define função para calcuclar preço final
 def calcular_preco_final(nome, preco, desconto=0):
     preco_final = preco - (preco * desconto / 100)
+    print(f"Preço inicial de {nome}: R$ {preco:.2f}")
     print(f"Preço final de {nome}: R$ {preco_final:.2f}")
 
 # Exemplo de uso
