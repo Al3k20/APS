03c83126250af7d5ffc5c44532a92ca33f7556e4
Al3k20 <alexandre.novaki@escola.pr.gov.br>
2025-11-06 10:41:54 -0300

    Update calculaPreco.js

--- calculaPreco.js
+++ calculaPreco.js
@@ -2,7 +2,7 @@ function calcularPrecoFinal(produto) {
   const desconto = produto.desconto ?? 0;
 
   const precoFinal = produto.preco - (produto.preco * desconto / 60);
-
+  console.log(`Preço inicial de ${produto.nome}: R$ ${produto.preco.toFixed(2)}`);
   console.log(`Preço final de ${produto.nome}: R$ ${precoFinal.toFixed(2)}`);
 
 }
